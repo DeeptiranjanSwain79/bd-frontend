c28926e6f042a32a44c21c264e3cc257fb4a9d5b
Deeptiranjana Swain <deeptiranjanswain777@gmail.com>
2025-03-01 13:17:57 +0530

    registration added

--- src/index.tsx
+++ src/index.tsx
@@ -14,3 +14,17 @@ root.render(
     </AuthContextProvider>
   </BrowserRouter>
 );
+
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then((registration) => {
+        console.log("Service Worker registered: ", registration);
+      })
+      .catch((registrationError) => {
+        console.log("Service Worker registration failed: ", registrationError);
+      });
+  });
+}
