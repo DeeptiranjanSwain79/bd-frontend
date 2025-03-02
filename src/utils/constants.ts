2a1df2c47cf5b7576bf6215b492ed2bda546968c
Deeptiranjana Swain <deeptiranjanswain777@gmail.com>
2025-03-02 11:17:12 +0530

    new baseURL added

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,3 @@
-// export const BASE_URL: string = "http://localhost:5000"
-export const BASE_URL: string = "https://bd-api-6l0w.onrender.com"
+// export const BASE_URL: string = "http://localhost:5000";
+// export const BASE_URL: string = "https://bd-api-6l0w.onrender.com";
+export const BASE_URL: string = "https://bd-api-alpha.vercel.app/";
